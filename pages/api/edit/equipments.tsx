d6a21006fb366af67cc2026d9a17e553197ff049
MintedKitten <mintycolo@gmail.com>
2022-09-07 15:23:48 +0700

    fix type

--- pages/api/edit/equipments.tsx
+++ pages/api/edit/equipments.tsx
@@ -30,6 +30,7 @@ const handler = nxcHandler().all(async (req, res) => {
         desc: row.desc,
         qty: row.qty,
         unitPrice: row.uPrice,
+        unit: row.unit,
       };
       isEditSuccessful =
         isEditSuccessful && (await equipmentsInsertOne(conn, query));
