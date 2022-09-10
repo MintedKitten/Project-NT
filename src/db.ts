4c8a135e9ea7a783b0d82e3868530b061fb1a942
MintedKitten <mintycolo@gmail.com>
2022-09-11 04:00:42 +0700

    small change

--- src/db.ts
+++ src/db.ts
@@ -50,13 +50,12 @@ export interface userInt {
 export async function getMongoClient() {
   const conn = await new MongoClient(`${process.env.NEXT_MONGO_STRING}`)
     .on("open", (mongoclient) => {
-      console.log(mongoclient.options);
+      console.log("A mongoclient has been opened");
     })
     .on("close", () => {
       console.log("A mongoclient has been closed");
     })
     .connect();
-
   return conn;
 }
 
